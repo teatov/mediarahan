01d3c5a755c8ea7d05b29cac64b4d6603c9e4cbe
teatov <hlebpoimenigleb@gmail.com>
2025-03-09 04:02:41 +1000

    add dark theme toggle

---
+++ src/lib/components/layout/ThemeToggle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { IconSun, IconMoon } from '@tabler/icons-svelte';
+
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Button onclick={toggleMode} variant="outline" size="icon">
+	<IconSun
+		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+	/>
+	<IconMoon
+		class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import ThemeToggle from '$lib/components/layout/ThemeToggle.svelte'
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+<ThemeToggle />
 {@render children()}
