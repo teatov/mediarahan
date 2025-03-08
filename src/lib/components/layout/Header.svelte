4f0bb06d5b3821b633809faf88b83e2077d8515e
teatov <hlebpoimenigleb@gmail.com>
2025-03-09 04:41:19 +1000

    добавлены шапка и подвал

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,6 @@
+<header class="bg-background border-b py-2">
+	<div class="container flex max-w-6xl items-center justify-between">
+		<a href="/">mediarahan</a>
+		<p>a</p>
+	</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button>Click me</Button>
+<main class="container max-w-xl">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<Button>Click me</Button>
+</main>
