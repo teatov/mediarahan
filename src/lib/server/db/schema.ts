144a247211976fed697e7c08127834a6c7246a0d
teatov <hlebpoimenigleb@gmail.com>
2025-03-09 06:33:43 +1000

    убрано поле `age` из таблицы `user`

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,8 +1,7 @@
-import { mysqlTable, int, varchar, datetime } from 'drizzle-orm/mysql-core';
+import { mysqlTable, varchar, datetime } from 'drizzle-orm/mysql-core';
 
 export const user = mysqlTable('user', {
   id: varchar('id', { length: 255 }).primaryKey(),
-  age: int('age'),
   username: varchar('username', { length: 32 }).notNull().unique(),
   passwordHash: varchar('password_hash', { length: 255 }).notNull(),
 });
