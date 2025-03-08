014713b3a391f329b7936a3101f4fb9209bfc8b0
teatov <hlebpoimenigleb@gmail.com>
2025-03-09 05:28:34 +1000

    добавлены переменные окружения для базы данных

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,10 +2,16 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = await mysql.createConnection(env.DATABASE_URL);
+
+const client = await mysql.createConnection({
+	database: env.DB_DATABASE,
+	user: env.DB_USER,
+	password: env.DB_PASSWORD,
+	host: env.DB_HOST,
+	port: env.DB_PORT
+});
 
 export const db = drizzle(client, {
-  schema,
-  mode: 'default'
+	schema,
+	mode: 'default'
 });
