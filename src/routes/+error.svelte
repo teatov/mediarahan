736301fe223edb61c8443ab0d3bb2396d65c1d45
teatov <hlebpoimenigleb@gmail.com>
2025-03-11 02:10:35 +1000

    добавлена страница ошибки

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from '$app/state';
+
+  let message = page?.error?.message;
+
+  if (page.status === 404) {
+    message = 'Такой страницы у нас нет';
+  }
+  if (page.status === 500) {
+    message = 'Произошло что-то страшное';
+  }
+</script>
+
+<div class="flex flex-col items-center">
+  <h1 class="text-6xl font-bold">{page?.status}</h1>
+  <p>{message}</p>
+</div>
