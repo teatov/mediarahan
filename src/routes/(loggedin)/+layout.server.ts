861dcc6675e1133e617bfefff1688235f54142b1
teatov <hlebpoimenigleb@gmail.com>
2025-03-11 04:17:22 +1000

    добавлена обработка привязки сервиса к существующему аккаунту

--- src/routes/(loggedin)/+layout.server.ts
+++ src/routes/(loggedin)/+layout.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-  if (!event.locals.user) {
+  if (!event.locals.session || !event.locals.user) {
     return redirect(302, '/login');
   }
 
