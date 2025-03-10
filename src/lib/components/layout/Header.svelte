e91830e67e91292cddfae8502b5bf20867356a7b
Teatov <hlebpoimenigleb@gmail.com>
2025-03-10 14:10:07 +1000

    добавлена страница входа

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import Logo from '$lib/components/layout/Logo.svelte';
+  import { Button } from '$lib/components/ui/button';
+  import { IconLogin } from '@tabler/icons-svelte';
 </script>
 
 <header class="bg-background border-b py-2">
   <div class="container flex max-w-6xl items-center justify-between">
     <a href="/" class="block font-bold"><Logo /><span class="px-1">mediarahan</span></a>
-    <p>a</p>
+    <div>
+      <Button href="/login"><IconLogin />Войти</Button>
+    </div>
   </div>
 </header>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+  import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root class="mx-auto w-[380px]">
+  <Card.Header>
+    <Card.Title>Войти или зарегистрироваться</Card.Title>
+  </Card.Header>
+  <Card.Content class="grid gap-4">
+    <Button class="w-full">Войти</Button>
+  </Card.Content>
+</Card.Root>
