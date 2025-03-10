5802e9714cdd7da301b2712848a4892071d5e6cb
Teatov <hlebpoimenigleb@gmail.com>
2025-03-10 18:19:14 +1000

    add basic profile page contents

--- src/lib/components/layout/Avatar.svelte
+++ src/lib/components/layout/Avatar.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import * as Avatar from '$lib/components/ui/avatar';
   import type { SessionValidationResult } from '$lib/server/auth';
+  import { Avatar as AvatarPrimitive } from 'bits-ui';
 
-  interface Props {
-    user: SessionValidationResult['user'];
-    [key: string]: any
-  }
-
-  let { user, ...rest }: Props = $props();
+  let {
+    user,
+    ...restProps
+  }: { user: SessionValidationResult['user'] } & AvatarPrimitive.RootProps = $props();
 </script>
 
-<Avatar.Root {...rest}>
+<Avatar.Root {...restProps}>
   <Avatar.Image src={user?.avatarUrl} alt={user?.username} />
   <Avatar.Fallback>{user?.username.substring(0, 2)}</Avatar.Fallback>
 </Avatar.Root>
