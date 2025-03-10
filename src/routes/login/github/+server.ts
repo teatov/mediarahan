cf01f950a21dfd2ef1dad04f298b1ab25e3cd1e2
Teatov <hlebpoimenigleb@gmail.com>
2025-03-10 23:32:31 +1000

    индивидуальные маршруты авторизации заменены на общий с параметром

--- src/routes/login/github/+server.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestEvent } from './$types';
-import { handleAuthRedirect } from '$lib/server/providers';
-import github from '$lib/server/providers/github';
-
-export function GET(event: RequestEvent): Response {
-  return handleAuthRedirect(github, event);
-}
--- src/routes/login/twitch/callback/+server.ts
+++
@@ -1,7 +0,0 @@
-import { handleAuthCallback } from '$lib/server/providers';
-import type { RequestEvent } from './$types';
-import twitch from '$lib/server/providers/twitch';
-
-export async function GET(event: RequestEvent): Promise<Response> {
-  return await handleAuthCallback(twitch, event);
-}
