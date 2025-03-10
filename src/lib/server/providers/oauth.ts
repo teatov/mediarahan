d6e6f62a414333c9be90515a43f4f130901cf096
Teatov <hlebpoimenigleb@gmail.com>
2025-03-10 21:13:22 +1000

    изменена организация `/server/providers`

--- src/lib/server/providers/oauth.ts
+++
@@ -1,8 +0,0 @@
-import { GitHub } from 'arctic';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, ORIGIN } from '$env/static/private';
-
-export const github = new GitHub(
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  ORIGIN + '/login/github/callback'
-);
