be86ac46d57e0c34f78fd3ee699bd1233ea5c450
Teatov <hlebpoimenigleb@gmail.com>
2025-03-11 15:58:42 +1000

    add list of websocket providers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export const providers = ['twitch', 'google', 'donationalerts', 'donatepay', 'github'] as const;
 export const authProviders: ProviderName[] = ['twitch', 'google', 'github'];
+export const socketProviders: ProviderName[] = ['donationalerts', 'donatepay'];
 
 export type ProviderName = (typeof providers)[number];
