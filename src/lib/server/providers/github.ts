986367e7cf1e42e59f4f2e9a9b28fa121b9e5058
Teatov <hlebpoimenigleb@gmail.com>
2025-03-11 16:08:28 +1000

    move provider methods inside their objects

--- src/lib/server/providers/github.ts
+++ src/lib/server/providers/github.ts
@@ -4,40 +4,37 @@ import type { Provider } from '$lib/server/providers';
 
 const oauth = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, ORIGIN + '/login/github/callback');
 
-function createAuthorizationURL(state: string) {
-  return oauth.createAuthorizationURL(state, []);
-}
-
-async function validateAuthorizationCode(code: string) {
-  try {
-    return await oauth.validateAuthorizationCode(code);
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
-async function getUserInfo(tokens: OAuth2Tokens) {
-  const userRequest = new Request('https://api.github.com/user');
-  userRequest.headers.set('Authorization', `Bearer ${tokens.accessToken()}`);
-  const userResponse = await fetch(userRequest);
-  const userResult = (await userResponse.json()) as {
-    login: string;
-    id: number;
-    avatar_url: string;
-  };
-
-  return {
-    externalUserId: String(userResult.id),
-    username: userResult.login,
-    avatarUrl: userResult.avatar_url,
-  };
-}
-
 export default {
   name: 'github',
   stateCookie: 'github_oauth_state',
-  createAuthorizationURL,
-  validateAuthorizationCode,
-  getUserInfo,
+
+  createAuthorizationURL: (state: string) => {
+    return oauth.createAuthorizationURL(state, []);
+  },
+
+  validateAuthorizationCode: async (code: string) => {
+    try {
+      return await oauth.validateAuthorizationCode(code);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  },
+
+  getUserInfo: async (tokens: OAuth2Tokens) => {
+    const userRequest = new Request('https://api.github.com/user');
+    userRequest.headers.set('Authorization', `Bearer ${tokens.accessToken()}`);
+    const userResponse = await fetch(userRequest);
+    const userResult = (await userResponse.json()) as {
+      login: string;
+      id: number;
+      avatar_url: string;
+    };
+
+    return {
+      externalUserId: String(userResult.id),
+      username: userResult.login,
+      avatarUrl: userResult.avatar_url,
+    };
+  },
 } as Provider;
