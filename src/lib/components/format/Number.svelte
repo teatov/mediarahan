9ba7636df52324ba637ef6ecee5a65504a16a2aa
Teatov <hlebpoimenigleb@gmail.com>
2025-03-14 17:42:28 +1000

    добавлены компоненты для форматирования дат и чисел

---
+++ src/lib/components/format/Number.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  let {
+    value,
+    currency,
+    ...restProps
+  }: HTMLAttributes<HTMLElement> & {
+    value: number;
+    currency?: string;
+  } = $props();
+</script>
+
+<data {...restProps} {value}
+  >{new Intl.NumberFormat(undefined, currency ? { style: 'currency', currency } : undefined).format(
+    value,
+  )}</data
+>
---
+++ src/lib/components/format/Time.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  let {
+    datetime,
+    type,
+    ...restProps
+  }: HTMLAttributes<HTMLElement> & {
+    datetime: Date;
+    type?: 'date' | 'time' | 'datetime';
+  } = $props();
+</script>
+
+<time {...restProps} datetime={datetime.toISOString()}
+  >{type === 'date'
+    ? datetime.toLocaleDateString()
+    : type === 'time'
+      ? datetime.toLocaleTimeString()
+      : datetime.toLocaleString()}</time
+>
