7ba1a60a1e5c47e06ec5afe7f96379c050786a70
teatov <hlebpoimenigleb@gmail.com>
2025-03-22 04:23:58 +1000

    add anchor component

---
+++ src/lib/components/typography/A.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+  import { cn } from '$lib/utils';
+
+  let { class: className, children, href, ...restProps }: HTMLAnchorAttributes = $props();
+</script>
+
+<a
+  class={cn('font-medium text-primary underline underline-offset-4', className)}
+  {href}
+  {...restProps}
+  target={href?.startsWith('/') ? undefined : '_blank'}
+  rel={href?.startsWith('/') ? undefined : 'noreferrer noopener'}>{@render children?.()}</a
+>
