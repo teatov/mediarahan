e400bb9eb3f207af965d39a0821f1756a32426a9
teatov <hlebpoimenigleb@gmail.com>
2025-03-22 03:32:51 +1000

    move logout action to `/profile`

--- src/routes/(loggedin)/profile/+page.server.ts
+++ src/routes/(loggedin)/profile/+page.server.ts
@@ -1,7 +1,8 @@
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import * as auth from '$lib/server/auth';
 import db from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
 import type { PageServerLoad, Actions } from './$types';
@@ -24,6 +25,16 @@ export const load: PageServerLoad = async (event) => {
 };
 
 export const actions: Actions = {
+  logout: async (event) => {
+    if (!event.locals.session) {
+      return fail(401);
+    }
+
+    await auth.invalidateSession(event.locals.session.id);
+    auth.deleteSessionTokenCookie(event);
+    return redirect(302, '/login');
+  },
+
   editProfile: async (event) => {
     if (!event.locals.session) {
       return fail(401);
