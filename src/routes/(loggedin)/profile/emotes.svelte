70b671bbecae3b01429fa4cbff4b4ad670f970f9
Teatov <hlebpoimenigleb@gmail.com>
2025-03-27 00:15:04 +1000

    add scroll area for emote list

--- src/routes/(loggedin)/profile/emotes.svelte
+++ src/routes/(loggedin)/profile/emotes.svelte
@@ -5,6 +5,7 @@
   import { buttonVariants } from '$lib/components/ui/button';
   import * as Dialog from '$lib/components/ui/dialog';
   import * as Form from '$lib/components/ui/form';
+  import { ScrollArea } from '$lib/components/ui/scroll-area/index';
   import type { EmoteSet } from '$lib/emote';
   import { cn } from '$lib/utils';
 
@@ -29,20 +30,22 @@
       <Dialog.Title>Смайлики</Dialog.Title>
     </Dialog.Header>
 
-    <ul class="space-y-4">
-      {#each emotes as emoteSet}
-        <li class="space-y-2">
-          <p class="font-semibold">{emoteSet.label}</p>
-          <ul class="flex flex-wrap gap-2">
-            {#each emoteSet.emotes as emote}
-              <li>
-                <Emote {emote} class="size-8" />
-              </li>
-            {/each}
-          </ul>
-        </li>
-      {/each}
-    </ul>
+    <ScrollArea class="border max-h-96">
+      <ul class="space-y-4">
+        {#each emotes as emoteSet}
+          <li class="space-y-2 p-2">
+            <p class="font-semibold">{emoteSet.label}</p>
+            <ul class="flex flex-wrap gap-2">
+              {#each emoteSet.emotes as emote}
+                <li>
+                  <Emote {emote} class="size-8" />
+                </li>
+              {/each}
+            </ul>
+          </li>
+        {/each}
+      </ul>
+    </ScrollArea>
 
     <form method="POST" action="?/updateEmotes" use:enhance>
       <Form.Button disabled={$submitting} class="w-full">
