fd58e65a3dc84ee7b5fde01e18059ec9658564c8
Teatov <hlebpoimenigleb@gmail.com>
2025-03-29 23:49:02 +1000

    switch sun and moon icons in theme toggle

--- src/lib/components/layout/theme-toggle.svelte
+++ src/lib/components/layout/theme-toggle.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <Button onclick={toggleMode} variant="outline" size="icon" title="Переключить светлую/тёмную тему">
-  <IconSun class="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-  <IconMoon class="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+  <IconMoon class="scale-100 dark:scale-0" />
+  <IconSun class="absolute scale-0 dark:scale-100" />
   <span class="sr-only">Toggle theme</span>
 </Button>
